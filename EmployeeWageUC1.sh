0d3014123af5b3c2ac134b2e952dc2703fad1e5c
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-23 17:05:00 +0530

    Check Employee is Present or Absent Using Random Function

---
+++ EmployeeWageUC1.sh
@@ -0,0 +1,25 @@
+using System;
+
+namespace EmployeeWageUC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Constant
+            int IsFullTime = 1;
+            Random random = new Random();
+            // Computation
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IsFullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
